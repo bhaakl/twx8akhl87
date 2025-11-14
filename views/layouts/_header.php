fff410a112d5117db3569f3b220b56b63aeb60a1
Baiysh Begaliev <bhaakl@ya.ru>
2025-11-14 17:17:48 +0300

    fixes;

--- views/layouts/_header.php
+++ views/layouts/_header.php
@@ -15,21 +15,6 @@ use yii\bootstrap5\Html;
     ]);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest
-                ? ['label' => 'Login', 'url' => ['/site/login']]
-                : '<li class="nav-item">'
-                    . Html::beginForm(['/site/logout'])
-                    . Html::submitButton(
-                        'Logout (' . Yii::$app->user->identity->username . ')',
-                        ['class' => 'nav-link btn btn-link logout']
-                    )
-                    . Html::endForm()
-                    . '</li>'
-        ]
     ]);
     NavBar::end();
     ?>
